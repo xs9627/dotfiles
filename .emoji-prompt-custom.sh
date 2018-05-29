9027a9890080ec77c8d7e2266bdc55d1abae5f20
Sonic Xiang <sonic.xiang@activenetwork.com>
2018-05-29 09:44:40 +0800

    Update script

--- .emoji-prompt-custom.sh
+++ .emoji-prompt-custom.sh
@@ -1,7 +1,6 @@
 ALL_EMOJI=("🤖" "🍺" "🐙" "🐹" "🐼" "🍥" "🐶" "☕️" "👾" "🌀")
-RANDOM_EMOJI=${ALL_EMOJI[$RANDOM % ${#ALL_EMOJI[@]} ]}
-
 EMOJI_SUFFIX='\[\033[0;30m\]¸\[\033[0m\]'
-CURRENT_EMOJI() {
-    echo -e "$(echo $RANDOM_EMOJI)$EMOJI_SUFFIX"
+
+RANDOM_EMOJI() {
+    echo -e "${ALL_EMOJI[$RANDOM % ${#ALL_EMOJI[@]}]}$EMOJI_SUFFIX"
 }
